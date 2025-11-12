a002d505f64b05187368e59da1d233ec5794e333
wilber-buendata <wilber@buendata.com>
2025-11-12 13:04:48 -0500

    chore: update plugin version to 2025111200

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_assign_ai';
 $plugin->release = '1.0.0';
-$plugin->version = 2025110900;
+$plugin->version = 2025111200;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
