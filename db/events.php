34aa948bcb3d015a8b4f8a3da52551357974d80e
wilber-buendata <wilber@buendata.com>
2025-11-17 09:40:31 -0500

    feat: add submission_created event observer for AI processing of submissions

--- db/events.php
+++ db/events.php
@@ -26,6 +26,13 @@ defined('MOODLE_INTERNAL') || die();
 
 $observers = [
     [
+        'eventname' => '\mod_assign\event\submission_created',
+        'callback' => 'local_assign_ai\observer::submission_created',
+        'includefile' => '/local/assign_ai/classes/observer.php',
+        'internal' => false,
+        'priority' => 9999,
+    ],
+    [
         'eventname' => '\mod_assign\event\submission_graded',
         'callback' => 'local_assign_ai\observer::submission_graded',
         'includefile' => '/local/assign_ai/classes/observer.php',
