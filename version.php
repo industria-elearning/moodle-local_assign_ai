73a4977ed45e3a5ff2a27486f067dd70d84370e4
wilber-buendata <wilber@buendata.com>
2025-11-18 17:47:27 -0500

    chore: bump plugin version to 2025111801

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_assign_ai';
 $plugin->release = '1.0.0';
-$plugin->version = 2025111700;
+$plugin->version = 2025111801;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
